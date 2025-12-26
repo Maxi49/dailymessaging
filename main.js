1e5a4c33502daecfc930d68fbde213d6dedb031d
Maxi49 <91350274+Maxi49@users.noreply.github.com>
2025-12-25 22:54:24 -0300

    I DONT CARE ANYMORE

---
+++ main.js
@@ -0,0 +1,43 @@
+const Client = require('whatsapp-web.js').Client;
+const LocalAuth = require('whatsapp-web.js').LocalAuth;
+
+const qrcode = require('qrcode-terminal');
+const cron = require('node-cron');
+
+const phoneNumber = `${process.env.PHONE_NUMBER}@c.us`;
+
+const messages = [
+    'Recordá tomar la pastilla mi amor ❤️',
+    'Hora de la pastilla amor 💕',
+    'Pastillita del día mi chiquita preshiosha (yo te amo mas)❣️',
+    'No te olvides la pastilla my loveshito 💘'
+];
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+
+client.on('qr', (qr) => {
+    // Escaneás esto con el WhatsApp de tu celu
+    qrcode.generate(qr, {small: true});
+    console.log('Escaneá el QR con tu WhatsApp');
+});
+
+client.on('ready', () => {
+    console.log('Bot listo!');
+    
+    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+    cron.schedule('30 22 * * *', async () => {
+        console.log('Cron ejecutándose:', new Date().toLocaleString());
+        
+        try {
+            await client.sendMessage(phoneNumber, randomMessage);
+            console.log('Mensaje enviado!');
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    });
+});
+
+client.initialize();
